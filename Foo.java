89e922e8e77f1ce2a09a712356007a82032ecf6c
Rifat <31696555+rifatSDAS@users.noreply.github.com>
2022-03-28 13:53:51 +0200

    Add files via upload
    A test java class for tutorial

---
+++ Foo.java
@@ -0,0 +1,12 @@
+package com.company.foo;
+
+public class Foo {
+    public static void main(String[] args) {
+        // This is how to comments
+        /*
+        This is multi line comments
+        This is multi line comments
+         */
+        System.out.println("This is a foo foo class");
+    }
+}
